3ec31f78a78482ed9ff09bcc10512064dad3f621
Alvi <mohalviridho@gmail.com>
2023-03-26 02:55:11 +0700

    role beres

--- middleware/auth.js
+++ middleware/auth.js
@@ -94,3 +94,7 @@ exports.login = function (req, res) {
     }
   });
 };
+
+exports.rahasia = function (req, res) {
+  response.ok("Halaman ini hanya untuk user dengan role = 2!", res);
+};
--- middleware/index.js
+++ middleware/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var auth = require('./auth');
+var verifikasi = require('./verifikasi');
 var router = express.Router();
+
 
 // Daftar menu Registrasi
 router.post('/api/v1/register', auth.register);
 router.post('/api/v1/login', auth.login);
+
+// Alamat yang perlu otorisasi
+router.get("/api/v1/rahasia", verifikasi(), auth.rahasia);
 
 module.exports = router;
